0eb8928a10149decbc11271b4ea663f75ca63d67
lawandia <111422631+lowondio@users.noreply.github.com>
2026-08-19 01:29:28 +0300

    Update serve_model.py

--- src/serve_model.py
+++ src/serve_model.py
@@ -3,36 +3,29 @@ from pydantic import BaseModel
 import joblib
 import numpy as np
 
-# 1. Инициализируем приложение FastAPI
-app = FastAPI(title="ML Prediction API", description="API для нашей первой ML-модели")
+app = FastAPI(title="ML prediction API", description="API for first ML model")
 
-# Глобальная переменная для хранения модели
 model = None
 
-# 2. Загружаем модель при старте сервера
 @app.on_event("startup")
 def load_model():
     global model
     try:
         model = joblib.load("models/model.pkl")
-        print("✅ Модель успешно загружена в память!")
+        print("Model loaded")
     except Exception as e:
-        print(f"❌ Ошибка загрузки модели: {e}")
+        print(f"Fail: {e}")
 
-# 3. Описываем схему входящих данных (Pydantic валидация)
-# Наша синтетическая выборка генерировала 15 признаков
 class PredictRequest(BaseModel):
     features: list[float]
 
-# 4. Создаем endpoint для предсказаний
 @app.post("/predict")
 def predict(request: PredictRequest):
     if model is None:
-        raise HTTPException(status_code=500, detail="Модель не загружена")
+        raise HTTPException(status_code=500, detail="The model has not been loaded")
     
-    # Проверяем количество признаков
     if len(request.features) != 15:
-        raise HTTPException(status_code=400, detail="Ожидается массив из 15 чисел")
+        raise HTTPException(status_code=400, detail="An array of 15 numbers is expected")
     
     input_data = np.array([request.features])
     prediction = model.predict(input_data)
