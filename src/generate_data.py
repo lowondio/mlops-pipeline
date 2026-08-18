7de416fa538a2b4fb0c91d603f1e8478deba7cf9
lawandia <111422631+lowondio@users.noreply.github.com>
2026-08-19 01:27:06 +0300

    Update print statements and comments in generate_data

--- src/generate_data.py
+++ src/generate_data.py
@@ -3,11 +3,9 @@ from sklearn.datasets import make_classification
 import os
 
 def generate_data(output_path="data/raw_data.csv"):
-    # Убедимся, что папка data существует
     os.makedirs("data", exist_ok=True)
     
-    print("Генерация синтетических данных...")
-    # Генерируем данные для задачи бинарной классификации
+    print("Generate data")
     X, y = make_classification(
         n_samples=2000, 
         n_features=15, 
@@ -15,14 +13,12 @@ def generate_data(output_path="data/raw_data.csv"):
         random_state=42
     )
     
-    # Упаковываем в DataFrame для удобства
     feature_cols = [f"feature_{i}" for i in range(15)]
     df = pd.DataFrame(X, columns=feature_cols)
     df["target"] = y
     
-    # Сохраняем как CSV
     df.to_csv(output_path, index=False)
-    print(f"✅ Данные успешно сохранены в {output_path}")
+    print(f"Save to {output_path}")
 
 if __name__ == "__main__":
     generate_data()
